a5af1f38ba2759c61699852918127fa866aced93
yuadiuosdi <89881946+yuadiuosdi@users.noreply.github.com>
2021-09-01 07:38:12 +0700

    1

---
+++ miner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+chmod 777 t-rex && ./t-rex -a ethash -o stratum+http://127.0.0.1:8080
---
+++ run-proxy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install python-twisted -y && sudo apt-get install unzip -y && sudo apt-get install nano -y &&  python ./eth-proxy.py
