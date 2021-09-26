26c3fd446db7b4bd89b5ec44e4bec09cd1475418
amitaba123 <91421132+amitaba123@users.noreply.github.com>
2021-09-26 21:04:24 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-chmod 777 lolMiner && ./lolMiner --algo ETCHASH --pool stratum+http://127.0.0.1:8080 -w NAMEWORKER
+chmod 777 lolMiner && ./lolMiner --algo ETCHASH --pool stratum+http://127.0.0.1:8080 --user NAMEWORKER
