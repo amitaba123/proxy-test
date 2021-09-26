6729b77153f996d0b46fed52b6b1dff7e674f7c4
amitaba123 <91421132+amitaba123@users.noreply.github.com>
2021-09-26 21:01:19 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-chmod 777 t-rex && ./t-rex -a ethash -o stratum+http://127.0.0.1:8080 -w NAMEWORKER
+chmod 777 lolMiner && ./lolMiner -a etchash -o stratum+http://127.0.0.1:8080 -w NAMEWORKER
